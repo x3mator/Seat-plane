778ca730afd80d12ca8ac67186ba1703ecdb6577
x3mator <x3mator@gmail.com>
2022-02-02 22:10:01 +0200

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+
+import getFormPerson from './modules/formPerson.js';
+import start from './modules/start.js';
+
+const init = (selectorApp, title) => {
+  const app = document.querySelector(selectorApp);
+
+  const {main, firstForm} = start(app, title);
+
+  firstForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const forms = getFormPerson(firstForm.count.value);
+    firstForm.remove();
+
+    main.append(...forms);
+  })
+};
+
+init('.app', 'Выберите тур');
